99ccea1efcfd93c68c5b8b6d54655d1764789cdf
Vildravn <vildravn@gmail.com>
2021-11-18 09:45:32 +0100

    Add a key hook to close backpacks on esc

--- mod.lua
+++ mod.lua
@@ -2,10 +2,12 @@ MOD_NAME = "more_backpacks"
 MOD_VERSION = "Alpha 0.1"
 MOD_AUTHOR = "Vildravn"
 
+OPEN_BACKPACKS = {}
+
 function register()
     return {
         name = MOD_NAME,
-        hooks = {"click"},
+        hooks = {"click", "key"},
         modules = {"dyestation", "backpacks", "definitions", "strings"}
     }
 end
@@ -43,11 +45,32 @@ function click(button, click_type)
                     menu_link_id = api_gp(backpacks[i]["menu_id"], "bp_link_id")
                     if menu_link_id == item_link_id then
                         open = api_gp(backpacks[i]["menu_id"], "open")
-                        api_toggle_menu(backpacks[i]["menu_id"], not open)
-                        api_sp(highlight_slot, "sprite_ref", (open == false and api_get_sprite("sp_" .. slot_item .. "_open") or api_get_sprite("sp_" .. slot_item)))
+                        if open then
+                            api_toggle_menu(backpacks[i]["menu_id"], false)
+                            api_sp(highlight_slot, "sprite_ref", api_get_sprite("sp_" .. slot_item))
+                            OPEN_BACKPACKS[backpacks[i]["menu_id"]] = nil
+                        else
+                            api_toggle_menu(backpacks[i]["menu_id"], true)
+                            api_sp(highlight_slot, "sprite_ref", api_get_sprite("sp_" .. slot_item .. "_open"))
+                            OPEN_BACKPACKS[backpacks[i]["menu_id"]] = highlight_slot
+                        end
                         break
                     end
                 end
+            end
+        end
+    end
+end
+
+function key(key_code)
+    if key_code == 27 then
+        for menu_id, slot_id in pairs(OPEN_BACKPACKS) do
+            open = api_gp(menu_id, "open")
+
+            if not open then
+                slot_item = api_gp(slot_id, "item")
+                api_sp(slot_id, "sprite_ref", api_get_sprite("sp_" .. slot_item))
+                OPEN_BACKPACKS[menu_id] = nil
             end
         end
     end
